c17dbb4042c87e979364eacfe8ed05979611235d
MatheusRibak <matheusribak@hotmail.com>
2020-05-06 20:54:51 -0300

    Alterações no Faq e Sobre

--- src/views/about-us/index.js
+++ src/views/about-us/index.js
@@ -24,7 +24,11 @@ const AboutUs = () => {
         Sobre nós
       </Typography>
       <Typography className={styles.description} component="p" color="primary">
-        Aqui vai o sobre nós
+        Com a chegada do Covid-19 vários comércios tiveram que ser fechados ao redor do mundo. O Mercado Sucupira em Cabo Verde não foi diferente.
+      <br />
+Devido a isto criamos o Sucupira Digital onde conectados os comerciantes com os seus clientes.
+<br />
+Pelo site será possivel que os clientes encontrem os comerciantes cadastrados e entrem em contatos com os mesmos para realizar seus pedidos.
       </Typography>
     </Grid>
   )
--- src/views/faq/index.js
+++ src/views/faq/index.js
@@ -25,7 +25,37 @@ const Faq = () => {
       <Typography component="h1" color="primary" variant="h1">
         Perguntas Frequentes
       </Typography>
-     
+
+
+      <Grid className={styles.description} color="primary">
+        <Typography component="p" variant="h2">
+          Sou Comerciante, como posso incluir o meu negócio?
+        </Typography>
+        Você clica em cadastrar, responde o questionário e manda algumas fotos dos seus produtos.
+        <Typography component="p" variant="h2">
+          Sou Cliente, como posso realizar e pagar pelo meu pedido?
+        </Typography>
+        Ainda não realizados pedidos ou pagamentos pelo site, você terá que ligar para o comerciantes e combinar diretamente com ele.
+
+        <Typography component="p" variant="h2">
+          Meu comércio não está na página principal, como posso achá-la?
+        </Typography>
+        A página principal só mostra 12 comércios por vez e cada vez que você atualiza o site, as opções são diferentes. Você consegue achar a sua página filtrando pelo campo departamento que ela está cadastrada!
+        <Typography component="p" variant="h2">
+          Sou Comerciante, como faço para editar meu cadastro?
+        </Typography>
+        Infelizmente não, seus dados vão direto para a página. Se quiser fazer alguma alteração você terá que preencher um novo cadastro e nos avisar no campo de observações.
+
+        <Typography component="p" variant="h2">
+          Como surgiu o Sucupira Digital?
+        </Typography>
+        Fomos participantes do Global Online Startup Weekend Covid-19 de Cabo Verde, onde fomos vencedores na área Empresas. Após isto decidimos continuar com o projeto visando auxiliar os comerciantes do mercado sucupira.
+        <Typography component="p" variant="h2">
+          Possui outras duvidas?
+        </Typography>
+        Iremos lhe auxiliar com o maior prazer, nos envie um email para sucupiradigital2020@gmail.com
+      </Grid>
+
     </Grid>
   )
 }
